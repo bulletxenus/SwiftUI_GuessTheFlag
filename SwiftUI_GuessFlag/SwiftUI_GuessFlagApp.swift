8c3e3a1a9b3d70dcb8064f8dbf1e38fcd6d3d79f
Dzmitry Khamitsevich <dzmitry_khamitsevich@epam.com>
2023-12-06 13:52:03 +0200

    Initial Commit

---
+++ SwiftUI_GuessFlag/SwiftUI_GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GuessFlagApp.swift
+//  SwiftUI_GuessFlag
+//
+//  Created by Dzmitry Khamitsevich on 6.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
